4eb7668c62b234c48a87719843589eee7045d6d9
KitsuneLenin <lenin11_2004@hotmail.com>
2025-02-18 16:07:09 -0500

    Estructura añadida

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/*
+<button class="button-add" onclick="agregarAmigo()">Añadir</button>
+
+Clase: button-add
+Este boton añadira los nombre de "amigos" a la lista.
+---
+<button class="button-draw" onclick="sortearAmigo()" aria-label="Sortear amigo secreto">
+
+Clase: button-draw
+Este boton sorteara en de forma alazar un nombre de los cuales fueron digitados.
+*/
+
+let NombreAmigos = [];
+
+// Agregar nombres
+
+function añadirNombres{
+
+}
+
+// Validar entrada
+
+function validarIngresoNombres{
+
+}
+
+
+// Visualizar la lista
+
+function listaDeNombres{
+
+}
+
+// Sorteo aleatorio
+
+function sorteoAleatorioDeNombres{
+
+}
